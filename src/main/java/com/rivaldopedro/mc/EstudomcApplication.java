706ded3ec2e8adc4bbbf9ea150e1709e6903fd62
Rivaldo Pedro <rivaldoagripino@gmail.com>
2021-06-15 17:53:14 -0300

    Criando operação de instânciação

--- src/main/java/com/rivaldopedro/mc/EstudomcApplication.java
+++ src/main/java/com/rivaldopedro/mc/EstudomcApplication.java
@@ -1,13 +1,35 @@
 package com.rivaldopedro.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudomcApplication {
+import com.rivaldopedro.mc.domain.Categoria;
+import com.rivaldopedro.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
